a35f46efc086f1d8f6f05ebd51b39d4918b37a94
yibantianxia <723609220@qq.com>
2021-05-12 15:01:11 +0800

    Olap kp dev Q7 Q9 (#73)
    * add parallel query code
    
    * add parallel query code
    
    * add parallel query code

--- sql/pq_clone_item.cc
+++ sql/pq_clone_item.cc
@@ -326,6 +326,16 @@ PQ_CLONE_DEF(Item_default_value) {
   }
 PQ_CLONE_RETURN
 
+Item *Item_view_ref::pq_clone(class THD *thd, class SELECT_LEX *select) {
+  Item_view_ref *new_item = new (thd->pq_mem_root)
+      Item_view_ref(&select->context, nullptr, table_name, orig_table_name,
+          field_name, cached_table);
+  Item** item_ref = new (thd->pq_mem_root) Item*();
+  *item_ref = (*ref)->pq_clone(thd, select);
+  new_item->ref = item_ref;
+  return new_item;
+}
+
 Item *Item_ref::pq_clone(class THD *thd, class SELECT_LEX *select) {
   /*
    * c1: (Name_resolution_context, db_name, table_name, field_name)
